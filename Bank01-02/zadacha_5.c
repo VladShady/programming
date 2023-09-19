fe036bb05900f0d8920ce425d5a11e098c1ee4f7
VladShady <67961109+VladShady@users.noreply.github.com>
2023-09-19 21:22:09 +0300

    Add files via upload

--- Bank01-02/zadacha_5.c
+++ Bank01-02/zadacha_5.c
@@ -1,28 +1,22 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() {
-    float x, y;
-    float res1, res2;
-
-    printf("Введіть значення x: ");
+int main()
+{
+    float z1, z2, x;
+    
+    printf("Введіть X: ");
     scanf("%f", &x);
-
-    printf("Введіть значення y: ");
-    scanf("%f", &y);
     
-    float res1_p1 = pow((cos(x) - cos(y)), 2);
-    float res1_p2 = pow((sin(x) - sin(y)), 2);
-
-    res1 = res1_p1 - res1_p2;
+    float z1_1 = sin(2 * x) + sin(5 * x) - sin(3 * x);
+    float z1_2 = cos(x) + 1 - 2 * pow(sin(2 * x), 2);
     
-    float res2_p1 = -4 * pow(sin((x - y) / 2), 2);
-    float res2_p2 = cos(x + y);    
-
-    res2 = res2_p1 * res2_p2;
-
-    printf("Результат за першою формулою: %f\n", res1);
-    printf("Результат за другою формулою: %f\n", res2);
+    z1 = z1_1 / z1_2;
+    
+    z2 = 2 * sin(x);
+    
+    printf("z1 = %f\n", z1);
+    printf("z2 = %f\n", z2);
 
     return 0;
 }
