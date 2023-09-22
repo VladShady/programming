a6ea02bb029f032452a9b9e34784d651cc799de1
VladShady <67961109+VladShady@users.noreply.github.com>
2023-09-22 17:28:23 +0300

    Add files via upload

--- Bank03-04/lab3_zadacha1.c
+++ Bank03-04/lab3_zadacha1.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
-int main() {
-    double distance; // Відстань в кілометрах
-    double time;     // Час в годинах
-    double speed;    // Швидкість в км/год
+int main()
+{
+    double distance;
+    double time;
+    double speed;
 
-    // Запит відстані та часу від користувача
     printf("Введіть відстань (км): ");
     scanf("%lf", &distance);
     printf("Введіть час (год): ");
     scanf("%lf", &time);
 
-    // Розрахунок швидкості
     speed = distance / time;
 
-    // Перевірка, чи порушено правила дорожнього руху
-    if (speed <= 60.0) {
+    if (speed <= 60.0)
+    {
         printf("Водій не порушив правил дорожнього руху.\n");
-    } else {
+    }
+    else
+    {
         printf("Водій порушив правила дорожнього руху.\n");
     }
 
